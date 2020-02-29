20acc61c1ef296da6bd83953305e496c7b6ad958
jonah-harvard <jfried@college.harvard.edu>
2020-02-28 20:32:59 -0500

    Scraping for harvard

--- scrape_rate_my_professor.py
+++ scrape_rate_my_professor.py
@@ -3,7 +3,9 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-
+import json
+from tqdm import tqdm
+from csv import DictReader
 
 SITE = "https://www.ratemyprofessors.com"
 HARVARD = f"{SITE}/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=Harvard+University&schoolID=399&query=*"
@@ -13,40 +15,61 @@ def parse_page(soup):
     professors = soup.find_all(class_="listing PROFESSOR")
     professor_links = [prof.find("a").get("href") for prof in professors]
     # print(professor_links)
-    professor_info = {}
-    for link in professor_links:
+    professor_info = []
+    strip_department = re.compile(" department")
+    for link in tqdm(professor_links, desc="Parsing Page"):
         prof_response = requests.get(f"{SITE}{link}")
         prof_soup = BeautifulSoup(prof_response.content, "html.parser")
+
         name = prof_soup.find(class_="NameTitle__Name-dowf0z-0 cjgLEI")
+
         rating = prof_soup.find(
             class_="RatingValue__Numerator-qw8sqy-2 gxuTRq"
         )
-        if rating == None:
-            continue
-        difficulty = prof_soup.find(
+
+        difficulty = prof_soup.find_all(
             class_="FeedbackItem__FeedbackNumber-uof32n-1 bGrrmf"
         )
-        if difficulty == None:
+
+        department = strip_department.sub("", prof_soup.find("b").text)
+
+        if name == None or rating == None or difficulty == []:
             continue
+
+        if len(difficulty) == 1:
+            difficulty_text = difficulty[0].text
+        else:
+            difficulty_text = difficulty[1].text
         # print(name)
-        professor_info[name.text] = {
-            "rating": rating.text,
-            "difficulty": difficulty.text
-        }
+        professor_info.append({
+            "name": name.text,
+            "rating": float(rating.text),
+            "difficulty": float(difficulty_text),
+            "department": department
+        })
     return professor_info
 
 
-if __name__ == "__main__":
-    response = requests.get(HARVARD)
+def parse_college(url):
+    response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     pages = soup.find_all(class_="step")
     page_links = [page.get("href") for page in pages]
-    professors = [parse_page(soup)]
-    print(professors)
-    for page_link in page_links:
+    professors = parse_page(soup)
+    for page_link in tqdm(page_links, desc="Parsing College"):
+        if page_link == None:
+            continue
         page_response = requests.get(f"{SITE}{page_link}")
         page_soup = BeautifulSoup(page_response.content, "html.parser")
-        prof = parse_page(page_soup)
-        print(prof)
+        professors += parse_page(page_soup)
+    return professors
 
-# TODO make sure links are not none
+
+if __name__ == "__main__":
+    with open("./college-salaries/salaries-by-college-type.csv") as csvfile:
+        reader = DictReader(csvfile)
+        colleges = [row["School Name"] for row in reader]
+    profs_by_college = {}
+    profs_by_college["Harvard"] = parse_college(HARVARD)
+    with open("professor_ratings_by_college.json", "w+") as f:
+        f.write(json.dumps(profs_by_college))
